c3324268283d04bccfa437b68e3040b5db8356b9
Tim Anderson <tdanderson805@gmail.com>
2020-10-26 22:56:37 -0700

    Working on LinkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,11 +2,30 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  list.length = 0;
+
+  // list = {head: { value: 5, next: { value: 6, next: {value: 7}}}
 
   list.addToTail = function(value) {
+    var newLink = Node(value);
+    //list.tail = newLink;
+    if (list.tail === null) {
+      list.head = newLink;
+      list.tail = newLink;
+    } else {
+      list.tail.next = newLink;
+
+    }
+
   };
 
   list.removeHead = function() {
+    console.log('here');
+    var headValue = list.head.value; // 5
+    var currentNode = list.head; // { value: 5, next: { value: 6, next: {value: 7}}
+    list.head = currentNode.next; // { value: 6, next: {value: 7}}
+
+    return headValue;
   };
 
   list.contains = function(target) {
