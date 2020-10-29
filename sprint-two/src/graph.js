3896e2cc225d536420c015e0fa11e38f1b67faa7
Tim Anderson <tdanderson805@gmail.com>
2020-10-28 21:00:37 -0700

    working on graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,30 +2,94 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  //this.edgeCount = 0;
+  this.adjacencyLists = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  if (!this.adjacencyLists[node]) {
+    this.adjacencyLists[node] = [];
+  }
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  // if key === node, return true
+  for (var key in this.adjacencyLists) {
+    // console.log( typeof key);
+    // console.log('this is the key ' + key);
+    // console.log(typeof node);
+    // console.log('this is the node ' + node);
+    if (key === node.toString()) {
+      return true;
+    }
+  }
+  return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  // step 1.  Remove Edges as well associated with the deleted node
+  // step2. Remove node
+  // look at this nodes adjacency lists
+  // Remove this node from those lists
+  // Finally remove the vertex/node
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  var fromNodeHasToNode = false;
+  var toNodeHasFromNode = false;
+  var fromArrayList = this.adjacencyLists[fromNode];
+  var toArrayList = this.adjacencyLists[toNode];
+  for (var i = 0; i < fromArrayList.length; i++) {
+    if (fromArrayList[i] === toNode.toString()) {
+      fromNodeHasToNode = true;
+      break;
+    }
+  }
+  for (var i = 0; i < toArrayList.length; i++) {
+    if (toArrayList[i] === toNode.toString()) {
+      toNodeHasFromNode = true;
+      break;
+    }
+  }
+  if (fromNodeHasToNode && toNodeHasFromNode) {
+    return true;
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  if (!this.adjacencyLists[fromNode]) {
+    this.addNode(fromNode);
+  }
+  if (!this.adjacencyLists[toNode]) {
+    this.addNode(toNode);
+  }
+
+  this.adjacencyLists[fromNode].push(toNode);
+  this.adjacencyLists[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  // adjList[fromNode] = [1,2,3, toNode, 4, 5]
+  // adjList[toNode] = [1,2,3, fromNode, 4, 5]
+  //first for loop
+  for (var i = 0; i < this.adjacencyLists[fromNode].length; i++) {
+    if (this.adjacencyLists[fromNode][i] === toNode) {
+      this.adjacencyLists[fromNode].splice(i, 1);
+    }
+  }
+  for (var i = 0; i < this.adjacencyLists[toNode].length; i++) {
+    if (this.adjacencyLists[toNode][i] === fromNode) {
+      this.adjacencyLists[toNode].splice(i, 1);
+    }
+  }
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
