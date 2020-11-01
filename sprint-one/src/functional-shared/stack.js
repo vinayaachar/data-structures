0bc7ccda81030d152e72480155d4b47c41f67205
vinayaachar <vinayaachar@gmail.com>
2020-10-31 19:03:30 -0700

    Complete sprint one

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,6 +1,35 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newMethods = {};
+  var temp;
+  extend(newMethods, stackMethods);
+  newMethods.count = 0;
+  newMethods.storage = {};
+  newMethods.temp = temp;
+
+  return newMethods;
+
 };
 
-var stackMethods = {};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+var stackMethods = {
+  push: function(value) {
+    this.count++;
+    this.storage[this.count] = value;
+
+  },
+  pop: function(value) {
+    this.temp = this.storage[this.count];
+    delete this.storage[this.count];
+    this.count--;
+    return this.temp;
+  },
+  size: function() {
+    return Object.keys(this.storage).length;
+  }
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,6 +1,12 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var temp;
+  const newMethods = Object.create(stackMethods);
+  newMethods.count = 0;
+  newMethods.storage = {};
+  newMethods.temp = temp;
+
+  return newMethods;
+
 };
 
 // Object to store stack methods
